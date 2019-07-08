71d83d6d1eb13e2570bba86182fe8a439e11a548
Lewis Franklin <lewis.franklin@rackspace.com>
2019-07-08 14:57:54 -0500

    Enhancement: pathlib for glossary.py

--- tests/test_glossary.py
+++ tests/test_glossary.py
@@ -1,6 +1,7 @@
 """Tests for glossary module."""
 from collections import defaultdict
 import copy
+import pathlib
 
 import pytest
 
@@ -15,9 +16,15 @@ def entry():
 @pytest.fixture()
 def step_glossary():
     old_value = copy.deepcopy(glossary.step_glossary)
-    glossary.step_glossary["Step one"].add_reference("Step one", "filename.feature", 12)
-    glossary.step_glossary["Step one"].add_reference("Step one", "filename2.feature", 2)
-    glossary.step_glossary["Step two"].add_reference("Step two", "filename.feature", 13)
+    glossary.step_glossary["Step one"].add_reference(
+        "Step one", pathlib.Path("filename.feature"), 12
+    )
+    glossary.step_glossary["Step one"].add_reference(
+        "Step one", pathlib.Path("filename2.feature"), 2
+    )
+    glossary.step_glossary["Step two"].add_reference(
+        "Step two", pathlib.Path("filename.feature"), 13
+    )
     yield
     glossary.step_glossary = old_value
 
@@ -29,13 +36,14 @@ def test_glossary_entry_init(entry):
 
 
 def test_glossary_entry_add_reference(entry):
-    entry.add_reference("A step", "filename.feature", 12)
+    feature_file = pathlib.Path("filename.feature")
+    entry.add_reference("A step", feature_file, 12)
     assert entry.step_set == {"A step"}
-    assert dict(entry.locations) == {"filename.feature": [12]}
+    assert dict(entry.locations) == {feature_file: [12]}
 
 
 def test_glossary_entry_tuple_len(entry):
-    entry.add_reference("A step", "filename.feature", 12)
+    entry.add_reference("A step", pathlib.Path("filename.feature"), 12)
     assert entry.tuple_len() == (1, 1)
 
 
