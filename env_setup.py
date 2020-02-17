c7c62edfee1067b6c56b25806c3f471fae6a236b
Brad Brown <brad@bradsbrown.com>
2020-02-17 15:10:35 -0600

    Enhancement: add extras to install

--- env_setup.py
+++ env_setup.py
@@ -12,7 +12,7 @@ import subprocess
 
 __commands_to_run = [
     "poetry run pip install --upgrade pip<19",
-    "poetry install",
+    "poetry install -E pytest-bdd",
     "poetry run pre-commit install",
 ]
 
